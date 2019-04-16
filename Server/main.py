a18f3a3467c6cbb34f77db8a276c567b06394c43
BiggyBaron <dr.cleverest@gmail.com>
2019-04-17 00:31:55 +0600

    Home folder fixed

--- Server/main.py
+++ Server/main.py
@@ -12,9 +12,11 @@ from flask import Flask, render_template, request, Markup, jsonify
 import time
 import json
 from tqdm import tqdm
+from pathlib import Path
 
 
 app = Flask(__name__)  # Creating new flask app
+home = str(Path.home())
 
 def write_json(data, name):
     with open(name, 'w') as outfile:
@@ -29,29 +31,29 @@ def read_json(name):
 
 @app.route("/buynode/", methods=["GET", "POST"])
 def dataCome():
-    data_show = read_json("./static/json/inside.json")
+    data_show = read_json(home + "/static/json/inside.json")
     data_show[0] += 1
-    write_json(data_show, "./static/json/inside.json")
+    write_json(data_show, home + "/static/json/inside.json")
     ts = time.time()*1000
     data2write = [ts, 1]
-    data_old = read_json("./static/json/data.json")
+    data_old = read_json(home + "/static/json/data.json")
     data_old.append(data2write)
-    write_json(data_old, "./static/json/data.json")
+    write_json(data_old, home + "/static/json/data.json")
     return "200"
 
 
 @app.route("/", methods=["GET", "POST"])
 def data():
-    data_show = read_json("./static/json/inside.json")
+    data_show = read_json(home + "/static/json/inside.json")
     data_show[1] += 1
-    write_json(data_show, "./static/json/inside.json")
+    write_json(data_show, home + "/static/json/inside.json")
 
     enters = data_show[1]
     craneNUM = data_show[0]
 
 
     print("Trying to read json")
-    data = read_json("./static/json/data.json")
+    data = read_json(home + "/static/json/data.json")
     for i in tqdm(range(len(data) - 1)):
         x = (data[i+1][0]/1000) - (data[i][0]/1000)
         if x>600:
@@ -65,7 +67,7 @@ def data():
                 change = ((data[i][0]/1000) + dif*60)
 
     data = sorted(data, key=lambda x: x[0])
-    write_json(data, "./static/json/data.json")
+    write_json(data, home + "/static/json/data.json")
     return render_template("index.html", **locals())
 
 
